66ff3bde991f4b6f5bdbc8864a9e02b9a01396ef
Ethan Patterson <edpatterson16@gmail.com>
2022-12-05 10:05:16 -0500

    My weird take on this

--- bestfriend.py
+++ bestfriend.py
@@ -1,2 +1,5 @@
 first_name = input("What is your name? ")
+friend = "Ethan"
 print(f"Hello {first_name}!")
+how_are_you = input("How are you doing today? ")
+print(f"I'm doing good; thank you for asking!")
